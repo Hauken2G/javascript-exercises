d1166c349e58d37a5e420505710fe99e475e2f5e
Hauken2G <kostasmichopoulos2@gmail.com>
2024-11-20 16:07:36 +0200

    remove / sum etc functions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function( start = 2.5 , finish = 4 ) {
+    let result = 0;
+for(i= start ; i <= finish ; i++ ) {
+    result += i;
+}
+return result;
 };
 
 // Do not edit below this line
